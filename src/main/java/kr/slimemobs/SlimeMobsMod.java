62eba46d229e99d566d34cbb9155eff27adfbce8
wlghgorcjswo <126782824+wlghgorcjswo@users.noreply.github.com>
2026-09-23 22:10:05 +0900

    Add src/main/java/kr/slimemobs/SlimeMobsMod.java

---
+++ src/main/java/kr/slimemobs/SlimeMobsMod.java
@@ -0,0 +1,8 @@
+package kr.slimemobs;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class SlimeMobsMod implements ModInitializer {
+ public static final String MOD_ID="slimemobs";
+ @Override public void onInitialize() {}
+}
